4bd71eec9bf1eca0c73e078c7c718bace0f9d102
nicotognella <nicolastognella@gmail.com>
2021-05-09 16:02:52 +1000

    archivos

---
+++ app.js
@@ -0,0 +1,5 @@
+function miFu (p1, p2, ...otro){
+    console.log (otro)
+}
+
+miFu ('a', 'b','c', 'd', 's')
---
+++ index.js
@@ -0,0 +1,10 @@
+let suma = (n1 , n2) => n1 + n2;
+let resta = (n1 , n2) => n1 - n2;
+let multiplicacion = (n1 , n2) => n1 * n2;
+let division = (n1 , n2) => n1 / n2;
+
+let calculadora = (n1, n2, operacion) => operacion (n1, n2)
+
+console.log (calculadora (5, 2,suma))
+
+
